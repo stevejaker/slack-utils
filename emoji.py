4d0c3e6474a713abcf97d182eaafd2bc0323ec69
stevejaker <35681532+stevejaker@users.noreply.github.com>
2020-08-07 22:54:59 -0600

    Updated SlackEmoji docstring

--- emoji.py
+++ emoji.py
@@ -15,15 +15,15 @@ class SlackEmoji(object):
     Basic class designed to handle all aspects of Slack's custom emoji.
 
     Slack does not have any public API methods for non Enterprise Grid teams
-    other than emoji.list. This class exposes the prvate API methods
-    emoji.remove and emoji.add API methods which allows authorized users to
-    use their session tokens from the web browser.
+    other than emoji.list. This class exposes the hidden emoji.remove and 
+    emoji.add API methods which allows authorized users to use their session 
+    tokens (from the web browser) to add emojis.
 
-    The following functions are provided (only required positional args are listed):
-        add(image_source, emoji_name) --> Adds new emoji
+    The following methods are provided (only required positional args are listed):
+        add(image_source, emoji_name) --> Adds new emoji using an image url as a source
         alias(alias_name, emoji_name) --> Adds alias to existing emoji
-        remove(emoji_name)            --> Deletes existing emoji
-        test(image_source)            --> Tests Adding and Removing an emoji
+        remove(emoji_name)            --> Deletes existing emoji by name
+        test(image_source)            --> Tests Adding and Removing an emoji using an image url as a source
 
     A Slack Token with authorization to add emojis is required. Some Slack teams
     prevent users other than Team Owners and Admins from modifying emojis. In
@@ -37,6 +37,10 @@ class SlackEmoji(object):
 
     Once the token is added, the team's domain name will be retrieved for use in
     the emoji.remove and emoji.add methods.
+
+    Image resizing will be attempted to to make sure that excess borders will be 
+    removed. Once uploaded, Slack will attempt to further resize the images. So far,
+    this has proven effective at providing high quality emojis.
     """
     def __init__(self, token=None):
         self.token = token
